f0677204c966c66a23685026406aae37dbc14385
越越 <hellotracyw@163.com>
2021-06-11 16:30:23 +0800

    method.py根据python规则修改函数参数

--- method.py
+++ method.py
@@ -1,51 +1,58 @@
 import numpy as np
 from matrix import getA, getB, getC, getD
 
-
 # 1) 用幂法求矩阵的最大特征值及其特征向量
-def power_eng(pld, env, a, n):
+def power_eng(a):
     '''
-    :param pld:
-    :param env:
-    :param a:
-    :param n:
-    :return:
+    :param a: 需要计算的矩阵
+    :return: 成功时返回最大特征值pld和特征向量env；失败时返回串failed
     '''
-    pass
 
+    pld = None
+    env = None
+    failed = "失败"
+    n = a.shape[0]
+
+    return pld, env
 
 # 2) 用反幂法求矩阵的最小特征值及其特征向量
-def inv_power_eng(pld, env, a, n):
+def inv_power_eng(a):
     '''
-    :param pld:
-    :param env:
-    :param a:
-    :param n:
-    :return:
+    :param a: 需要计算的矩阵
+    :return: 成功时返回最小特征值pld和特征向量env；失败时返回串failed
     '''
-    pass
 
+    pld = None
+    env = None
+    failed = "失败"
+    n = a.shape[0]
+
+    return pld, env
 
 # 3) 用Jacobi方法求对称矩阵的全部特征值
-def jacobi_eng(ev, a, n):
+def jacobi_eng(a):
     '''
-    :param ev:
-    :param a:
-    :param n:
-    :return:
+    :param a: 需要计算的矩阵
+    :return: 成功时返回特征值ev；失败时返回串failed
     '''
-    pass
 
+    ev = None
+    failed = "失败"
+    n = a.shape[0]
+
+    return ev
 
 # 4) 用Gauss相似变换将矩阵就地转换为上Hessenberg矩阵
-def gauss_hessen(a, n):
+def gauss_hessen(a):
     '''
-    :param a:
-    :param n:
-    :return:
+    :param a: 需要计算的矩阵
+    :return: 返回上Hessenberg矩阵
     '''
-    pass
 
+    n = a.shape[0]
+    Hessenberg = None
+
+    return Hessenberg
 
 # 5) 矩阵特征值问题QR算法辅助函数
 def qr_aux(i, j):
@@ -57,4 +64,7 @@ def qr_aux(i, j):
     pass
 
 if __name__ == '__main__':
-    pass
+    A = getA()
+    B = getB()
+    C = getC()
+    D = getD()

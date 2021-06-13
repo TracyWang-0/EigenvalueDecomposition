131e07c1b567def4678bde3591cf291dc76fd82e
越越 <hellotracyw@163.com>
2021-06-13 20:26:46 +0800

    完成幂法和反幂法求最大和最小特征值及其特征向量 并且统计填写表格中所有信息

--- method.py
+++ method.py
@@ -1,43 +1,64 @@
 import numpy as np
 from matrix import getA, getB, getC, getD
+from time import *
+
 
 # 1) 用幂法求矩阵的最大特征值及其特征向量
 def power_eng(A):
     '''
     :param A: 需要计算的矩阵
-    :return: 成功时返回最大特征值m和特征向量x
+    :return: 成功时返回最大特征值pld和对应的特征向量env
     '''
 
-    m = None
-    x = None
+    # m = None
+    # x = None
     n = A.shape[0]
 
-    v = np.zeros(n)     # 初始化一个one-hot向量v作为随机向量
-    v[-1] = 1
+    v = np.ones(n)  # 初始化一个ones向量v作为v_0
+    # u = v / v.max()
 
-    last_m = 0      # 记录上一次的m的值
+    # last_m = 0      # 记录上一次的m的值
 
-    while True:
-        y = A * v
-        y = y[:, -1]
-        m = max(y)
-        x = y / m
+    for i in range(1000):
+        y = v / np.abs(v.max())
+        v = np.dot(A, y)
+        # y = np.dot(A, v)
+        # # print('y', y)
+        # m = y.max()
+        # x = y / m
+        #
+        # # print(m)
+        # # print(last_m)
+        #
+        # print(np.abs(m - last_m))
+        # # print('y', y)
+        # # print('m', m)
+        # # print('last_m', last_m)
+        #
+        # if np.abs(m - last_m) <= 1:
+        #     print('i', i)
+        #     break
+        #
+        # v = np.dot(A, v)       # 计算下一个v向量
+        # # print('v', v)
+        # last_m = m
 
-        if m - last_m <= 1e-6:
-            break
+    pld = v.max()
+    env = y
 
-        v = A * v       # 计算下一个v向量
+    return pld, env
 
-    return m, x
 
 # 2) 用反幂法求矩阵的最小特征值及其特征向量
 def inv_power_eng(A):
     '''
     :param A: 需要计算的矩阵
-    :return: 成功时返回最小特征值m和特征向量x
+    :return: 成功时返回最大特征值pld和对应的特征向量env
     '''
 
-    return power_eng(np.linalg.inv(A))
+    pld, env = power_eng(np.linalg.inv(A))
+
+    return 1/pld, env
 
 # 3) 用Jacobi方法求对称矩阵的全部特征值
 def jacobi_eng(A):
@@ -52,6 +73,7 @@ def jacobi_eng(A):
 
     return ev
 
+
 # 4) 用Gauss相似变换将矩阵就地转换为上Hessenberg矩阵
 def gauss_hessen(A):
     '''
@@ -64,6 +86,7 @@ def gauss_hessen(A):
 
     return Hessenberg
 
+
 # 5) 矩阵特征值问题QR算法辅助函数
 def qr_aux(i, j):
     '''
@@ -73,15 +96,35 @@ def qr_aux(i, j):
     '''
     pass
 
+
 if __name__ == '__main__':
     A = getA()
     B = getB()
     C = getC()
     D = getD()
-    for mat in [A, B, C, D]:
-        print('矩阵' + str(mat) + '最大特征值及其特征向量: ', power_eng(mat))
-        print('矩阵' + str(mat) + '最小特征值及其特征向量: ', inv_power_eng(mat))
 
+    # print(np.linalg.eigvals(A).max())
+    #
+    # print(power_eng(A, 1000))
 
+    # for i, mat in enumerate([A, B, C, D]):
+    #     pld, env = power_eng(mat)
+    #     st = time()
+    #     print('第' + str(i + 1) + '个矩阵最大特征值及其特征向量: ', pld.round(5), env.round(5))
+    #     et = time()
+    #     max_pld = np.linalg.eigvals(mat).max()
+    #     print('第' + str(i + 1) + '个矩阵的最大特征值误差: ', np.abs(max_pld - pld).round(5))
+    #     print('runtime: ', et - st)
+    #
+    #     pld, env = inv_power_eng(mat)
+    #     st = time()
+    #     print('第' + str(i + 1) + '个矩阵最小特征值及其特征向量: ', pld.round(5), env.round(5))
+    #     et = time()
+    #     inv_mat = np.linalg.inv(mat)
+    #     min_pld = np.linalg.eigvals(inv_mat).min()
+    #     # print(min_pld)
+    #     print('第' + str(i + 1) + '个矩阵的最小特征值误差: ', np.abs(min_pld - pld).round(5))
+    #     print('runtime: ', et - st)
 
-
+    for i, mat in enumerate([A, B, C, D]):
+        pass
